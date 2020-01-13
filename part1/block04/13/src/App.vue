f269087eafaf6c7ddc4b1ab83850912f1c964b5a
zmalakhov <zm.13.15.zm@gmail.com>
2020-01-13 14:05:42 +0300

    0037 - Блок 4 - урок 13 - Передача html компоненту

---
+++ part1/block04/13/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-car>
+      <h2 slot="title">{{carName}}</h2>
+      <p slot="text">Lorem ipsum dolor.</p>
+    </app-car>
+  </div>
+</template>
+
+<script>
+    import Car from './Car.vue'
+
+    export default {
+      data() {
+        return {
+          carName: 'Ford'
+        }
+      },
+      components: {
+        appCar: Car,
+      }
+    }
+</script>
+
+<style>
+  /*h2 {*/
+  /*  color: red;*/
+  /*}*/
+</style>
---
+++ part1/block04/13/src/Car.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <hr>
+    <hr>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+    padding: 10px;
+  }
+
+  h2 {
+    color: red;
+  }
+
+</style>
